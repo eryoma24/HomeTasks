dbbde8ef9a43f0fad0c8c8ace1370eccd78cd380
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-15 23:05:37 +0600

    Added second part of code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число #1 ");
+﻿Console.WriteLine("Введите число номер 1 => ");
 string inputedString1 = Console.ReadLine();
 int number1 = Convert.ToInt32(inputedString1);
 
+Console.WriteLine("Введите число номер 2 => ");
+string inputedString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString2);
