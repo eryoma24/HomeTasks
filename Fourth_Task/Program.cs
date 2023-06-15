84c10263702e7fc49cc4aaddab3107a93b35a600
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-15 23:55:59 +0600

    finished fourth task

---
+++ Fourth_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число (N) => ");
+string inputedString = Console.ReadLine();
+int N = Convert.ToInt32(inputedString);
+
+Console.WriteLine($"Каждое чётное число от 1 до {N} => ");
+
+int Z = 2;
+while (Z <= N)
+{
+    Console.WriteLine(Z);
+    Z += 2;
+}
