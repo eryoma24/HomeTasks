cfcce9595c9c9900789f15ee4fddae82a4673575
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-15 23:48:54 +0600

    Finished third task

---
+++ Third_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое число => ");
+string inputedString = Console.ReadLine();
+int number = Convert.ToInt32(inputedString);
+
+if (number %2 == 0)
+{
+    Console.WriteLine($"Число'{number}' четное");
+}
+else
+{
+    Console.WriteLine($"Число '{number}' нечётное");
+}
