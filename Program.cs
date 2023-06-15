48bc92e723a62f9c37ed7c2ef35fadb92bbd7518
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-15 23:08:12 +0600

    Открыли цикл

--- Program.cs
+++ Program.cs
@@ -5,3 +5,9 @@ int number1 = Convert.ToInt32(inputedString1);
 Console.WriteLine("Введите число номер 2 => ");
 string inputedString2 = Console.ReadLine();
 int number2 = Convert.ToInt32(inputedString2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} наибольшее число");
+    Console.WriteLine($"{number2} меньшее число");
+}
