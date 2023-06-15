b4edd6578a431cab744e0bc3800e0bfe137c29d0
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-15 23:29:46 +0600

    Finished second Task

---
+++ Second_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число номер 1 => ");
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);
+
+Console.WriteLine("Введите число номер 2 => ");
+string inputedString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString2);
+
+Console.WriteLine("Введите число номер 3 => ");
+string inputedString3 = Console.ReadLine();
+int number3 = Convert.ToInt32(inputedString3);
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+
+if (number3 > max)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Максимальное число равно {max}! ");
